c31211e9489927031c7df69b5d5b12c06d3580d7
Zeyneb Bayrak <85934122+zby-zy@users.noreply.github.com>
2022-03-21 11:15:00 +0300

    Add files

---
+++ ToDoProject/todo/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from django.http import HttpRequest, HttpResponse
+from .models import Todo
+
+
+def list_todo_items(request):
+    context = {'todo_list' : Todo.objects.all()}
+    return render(request, 'todo/todo_list.html',context)
+
+
+def insert_todo_item(request: HttpRequest):
+    todo = Todo(content=request.POST['content'])
+    todo.save()
+    return redirect('/todo/list/')
+
+def delete_todo_item(request,todo_id):
+    todo_to_delete = Todo.objects.get(id=todo_id)
+    todo_to_delete.delete()
+    return redirect('/todo/list/')
+
+    
+    
